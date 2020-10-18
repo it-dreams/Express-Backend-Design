c167f100a9771c03df46e0cd570481eeeb8a34de
it-dreams <rahulraj.holi888@gmail.com>
2020-10-18 12:11:49 +0530

    Add validation with register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const {
+    validationResult,
+    check
+} = require('express-validator')
 
-// @routes GET api/users
-// @desc Test Route
+// @routes POST api/users
+// @desc Register User
 // @access Public
-router.get("/", (req, res) => res.send("Users route"));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Enter a password with 6 and more characters')
+    .isLength({
+        min: 6
+    })
+], (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({
+            error: error.array()
+        })
+    }
+    res.send("User route")
+});
 
 module.exports = router;
